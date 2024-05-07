67d145067767bf7b74cedd8634550626be3e4f33
ChloeGGYY <97067927+ChloeGGYY@users.noreply.github.com>
2024-05-07 05:21:12 +0000

    delete

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,24 @@
+class PhotosController < ApplicationController
+  def index
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order({ :created_at => :desc})
+    render({ :template => "photo_templates/index"})
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({ :id => url_id })
+    @the_photo = matching_photos[0]
+    render({ :template => "photo_templates/show"})
+  end
+
+  def baii
+    the_id = params.fetch("toast_id")
+    matching_photos = Photo.where({ :id => the_id })
+    the_photo = matching_photos[0]
+    the_photo.destroy
+   
+    #render({ :template => "photo_templates/baii"})
+    redirect_to("/photos")
+  end
+end
